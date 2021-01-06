d436ca6d9fdd32e38ca2f97e8247a8fdfe37d08d
kinggeert <geertbruls@outlook.com>
2021-01-06 10:28:12 +0100

    started project

---
+++ src/main/java/me/kinggeert/fabricplaytime/fabricplaytime/FabricPlaytime.java
@@ -0,0 +1,16 @@
+package me.kinggeert.fabricplaytime.fabricplaytime;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.minecraft.server.MinecraftServer;
+
+public class FabricPlaytime implements DedicatedServerModInitializer {
+
+    @Override
+    public void onInitializeServer() {
+
+    }
+
+    private void tick(MinecraftServer server) {
+        
+    }
+}

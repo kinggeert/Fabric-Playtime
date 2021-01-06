869e615e2b7a5c41fb170de66937c4986b6113e3
kinggeert <geertbruls@outlook.com>
2021-01-06 10:31:42 +0100

    added runtime

--- src/main/java/me/kinggeert/fabricplaytime/fabricplaytime/FabricPlaytime.java
+++ src/main/java/me/kinggeert/fabricplaytime/fabricplaytime/FabricPlaytime.java
@@ -1,16 +1,17 @@
 package me.kinggeert.fabricplaytime.fabricplaytime;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.event.server.ServerTickCallback;
 import net.minecraft.server.MinecraftServer;
 
 public class FabricPlaytime implements DedicatedServerModInitializer {
 
     @Override
     public void onInitializeServer() {
-
+        ServerTickCallback.EVENT.register((MinecraftServer) -> tick(MinecraftServer));
     }
 
     private void tick(MinecraftServer server) {
-        
+
     }
 }
